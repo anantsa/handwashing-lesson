a736ed29be10bc4c29650c1c4f55088f135ac92d
anantsa <nepalianan@gmail.com>
2021-05-10 21:38:15 +0545

    got the timer code

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,14 +1,40 @@
 <script>
   import ProgressBar from "./ProgressBar.svelte";
+  const totalSeconds = 20;
+
+  let isRunning = false;
+  let secondLeft = totalSeconds;
+
+  $: progress = ((totalSeconds - secondLeft) / totalSeconds) * 100;
+
+  function startTimer() {
+    isRunning = true;
+    const timer = setInterval(() => {
+      secondLeft -= 1;
+
+      if (secondLeft == 0) {
+        clearInterval(timer);
+        isRunning = false;
+        secondLeft = totalSeconds;
+      }
+    }, 1000);
+  }
 </script>
 
 <div bp="grid">
-  <h2 bp="offset-5@md 4@md 12@sm">Seconds left:</h2>
+  <h2 bp="offset-5@md 4@md 12@sm">Seconds left:{secondLeft}</h2>
 </div>
 
-<ProgressBar />
+<ProgressBar {progress} />
 
-<button class="stretch primary">Start</button>
+<div bp="grid">
+  <button
+    bp="offset-5@md 4@md 12@sm"
+    class="stretch primary"
+    on:click={startTimer}
+    disabled={isRunning}>Start</button
+  >
+</div>
 
 <style>
   h2 {
